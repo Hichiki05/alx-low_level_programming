1a7684986799039630385028880117474db425d2
safae <safaeelbazofficial@gmail.com>
2023-06-26 17:53:44 -0700

    ll

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,30 +1,46 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * rot13 - encoder rot13
- * @s: pointer to string params
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: *s
+ * Return: the int converted from the string
  */
-
-char *rot13(char *s)
+int _atoi(char *s)
 {
-	int i;
-	int j;
-	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, d, n, len, f, digit;
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		for (j = 0; j < 52; j++)
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (s[i] == data1[j])
-			{
-				s[i] = datarot[j];
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			}
+			f = 0;
 		}
+		i++;
 	}
-	return (s);
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
