7710b3c750cf5475ca3a5bd82e53d2611adef92b
safae <safaeelbazofficial@gmail.com>
2023-06-30 16:39:38 -0700

    mm

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strcat - a fonction that concatenates two strings
+*
+* @src: the first string
+* @dest: the second string
+*
+*Return: the value of dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	char *original = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		*dest++;
+		*src++;
+	}
+	*dest = '\0';
+	return original;
+}
