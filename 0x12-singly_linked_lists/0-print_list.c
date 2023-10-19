70784048f93918bb46dca8ac90d9a8d8ea331bc0
Hichiki05 <safaeelbazofficial@gmail.com>
2023-10-19 11:03:09 -0700

    ll

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,13 @@
 #include <stdlib.h>
+#include "origin.h"
 
 /**
  * print_list - a fnct
  * @h: a pointer
  * Return: the nbr of nodes
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
  *
  * Description: singly linked list node structure
  */
-
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
 
 size_t print_list(const list_t *h)
 {
