a1649499cd1944fc12a7d4c353f2b8f5e4ff4da5
Hichiki05 <safaeelbazofficial@gmail.com>
2023-10-31 15:47:35 +0100

    file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,6 +9,8 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+void generate_password(char *password);
+void save_password(const char *password);
 
 #endif
 
